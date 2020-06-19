0d9e58587d15082753dd4f2c2bf1f8bd428089b0
wpdbsx <wpdbsx@naver.com>
2020-06-19 17:45:56 +0900

    올리기

---
+++ demo/src/main/java/com/example/security/CustomUserDetailService.java
@@ -0,0 +1,36 @@
+package com.example.security;
+
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.mapper.UserMapper;
+import com.example.vo.UserVo;
+
+@Service
+@Transactional
+public class CustomUserDetailService implements UserDetailsService{
+	
+	@Autowired
+	UserMapper user1mapper = null;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		//username을 이용하여 mapper에서 원하는 값을 받아옴.
+		UserVo user =user1mapper.findByUsername(username);
+		 String password= user.getPassword();
+		 String[] userRoles = {user.getAdmin()};
+		 Collection<GrantedAuthority> roles = AuthorityUtils.createAuthorityList(userRoles);
+			return new User(username,password,roles);
+	}
+
+}
---
+++ demo/src/main/java/com/example/security/SecurityConfig.java
@@ -0,0 +1,79 @@
+package com.example.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration  //환경설정파일 이라는뜻 서버가 돌기전에 먼저 완성되어야 하는것 서버가 돌기전에 하기 때문에 new를 쓰는게아니라 bean을써서 미리 만들어야한다.
+@EnableWebSecurity  //Security사용한다는뜻
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	//새롭게 만든 UserDetailsService를 사용함.
+	@Autowired
+	CustomUserDetailService userDetailsService;
+	
+	@Bean
+	public BCryptPasswordEncoder BCE() {
+		return new BCryptPasswordEncoder();
+	}
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		//새로만든 service객체 추가 + 암호화 방법 설정
+		auth.userDetailsService(userDetailsService).passwordEncoder(BCE());
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		//Security사 적용되지 않는 url
+		web.ignoring().antMatchers("/css/**","/script/**","/image/**","/fonts/**","/lib/**");
+		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests() 
+			 	.antMatchers("/admin","/admin/*").hasAuthority("ADMIN")
+			 	.anyRequest().permitAll()
+			 	.and()
+//			 .formLogin()
+//			 	.loginPage("/security/login")  //로그인 페이지 url
+//			 	.loginProcessingUrl("/security/loginProcess")//<form action= "" 설정
+//			 	.permitAll()    			//누구나 접근
+//			 	.defaultSuccessUrl("/security/home") //성공시 이동할 페이지
+//			 	.and()
+			 .formLogin()
+	        	.loginPage("/login")  //로그인 페이지 url
+	        	.loginProcessingUrl("/loginProcess") //<form action="?"
+	        	.permitAll()							//누구나 접근
+	        	.defaultSuccessUrl("/home") 	//성공시 이동할 페이지 
+	        	.and()
+			 .logout()
+			 	.logoutUrl("/logout")  //로그아웃페이지
+			 	.logoutSuccessUrl("/home") //성공시 페이지
+			 	.invalidateHttpSession(true)  // httpSession.invalidate 를 실행
+			 	.clearAuthentication(true)///로그아웃 후 인증삭제
+			 	.permitAll()
+			 	.and()
+			 .exceptionHandling()
+			 	.accessDeniedPage("/security/page403");
+		
+			//보안에 취약함.
+		    http.csrf().disable(); //csrf를 사용하지 않을 경우
+				
+		 	
+//		 	.authorizeRequests()
+//		 	.antMatchers("/security/manager","/security/manager/*").hasAuthority("MANAGER")
+//		 	.anyRequest().permitAll()
+//		 	.and()
+		 	
+	}
+
+	
+}
