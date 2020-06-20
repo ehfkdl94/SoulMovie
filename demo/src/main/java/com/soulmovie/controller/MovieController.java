02a2ae06dd91bf7af763014ae0df15f81592d6f2
wpdbsx <wpdbsx@naver.com>
2020-06-20 19:31:18 +0900

    .

--- demo/src/main/java/com/soulmovie/controller/MovieController.java
+++ demo/src/main/java/com/soulmovie/controller/MovieController.java
@@ -1,5 +1,7 @@
 package com.soulmovie.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -7,12 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.soulmovie.vo.MovieVO;
+
 @Controller
 @RequestMapping(value = "/movie")
 public class MovieController {
 	@RequestMapping(value = "/moviedetail", method = RequestMethod.GET)
 	public String moviedetail(HttpServletRequest request,
-			@RequestParam(value="page", defaultValue="1",required= false) int page) {
+			@RequestParam(value="movie_code", defaultValue="1",required= false) int code) {
+		List<MovieVO> list = mDAO.selectBoard();
+		model.addAttribute("list", list);
 		
 		return request.getContextPath()+"/movie/moviedetail";
 	}
