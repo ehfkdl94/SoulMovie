9c59dc5c2ad779da8931317f762beec7909bae3f
JungmiPark <53827648+JungmiPark@users.noreply.github.com>
2020-06-21 02:46:02 +0900

    Choice 0620
    영화 검색
    InsertChoice 기능 추가
    테스트 전

--- demo/src/main/java/com/soulmovie/mapper/ChoiceMapper.java
+++ demo/src/main/java/com/soulmovie/mapper/ChoiceMapper.java
@@ -1,5 +1,15 @@
 package com.soulmovie.mapper;
 
+import org.apache.ibatis.annotations.Insert;
+
+import com.soulmovie.vo.ChoiceVO;
+
 public interface ChoiceMapper {
 
+@Insert({"INSERT INTO CHOICE"
+		+ "(CHOICE_NO, CHOICE_FREQ, CHOICE_REASON, CHOICE_CATEGORY, CHOICE_DATE, CHOICE_CODE, CHOICE_ID)" + 
+		"VALUES (SEQ_CHOICE_NO.NEXTVAL, #{obj.choice_freq}, #{obj.choice_reason}," + 
+		"		#{obj.choice_category}, SYSDATE, #{obj.choice_code}, #{choice_id})"})
+public int insertChoice(ChoiceVO obj);
 }
+
