188c51ac2a4b50f357d498b66f6bfdf3f8202d46
yeongjin <syeongjin144@gmail.com>
2020-06-21 13:33:23 +0900

    12

--- demo/src/main/java/com/soulmovie/controller/BoardController.java
+++ demo/src/main/java/com/soulmovie/controller/BoardController.java
@@ -1,11 +1,19 @@
 package com.soulmovie.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -51,5 +59,121 @@ public class BoardController {
 		
 		return request.getContextPath()+"redirect:/";
 	}
+	
+	@RequestMapping(value = "/content", method = RequestMethod.GET)
+	public String content(Model model, HttpSession httpSession, HttpServletRequest request,
+			@RequestParam(value="no", defaultValue = "0", required = false) int no) {
+		if( no == 0) {
+			return request.getContextPath()+"redirect:/board/list";
+		}
+				
+		Integer chk = (Integer)httpSession.getAttribute("SESSION_BOARD_HIT_CHECK");
+		if (chk != null) {
+			if( chk == 1) {
+				bDAO.updateHit(no);
+				httpSession.setAttribute("SESSION_BOARD_HIT_CHECK", 0);
+			}
+		}
+		
+		BoardVO obj = bDAO.selectBoardOne(no);
+		model.addAttribute("obj", obj);
+		
+		int p = bDAO.selectBoardPrev(no);
+		model.addAttribute("prev", p);
+		
+		int n = bDAO.selectBoardNext(no);
+		model.addAttribute("next", n);
+		
+		return request.getContextPath()+"/board/content";
+	}
 
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String list(Model model, HttpSession httpSession, 
+			HttpServletRequest request,
+			@RequestParam(value="page", defaultValue = "0", required = false) int page,
+			@RequestParam(value="text", defaultValue = "", required = false) String text) {
+		
+		if(page == 0) {
+			return "redirect:" + request.getContextPath() + "/board/list?page=1"; 
+		}
+		
+		httpSession.setAttribute("SESSION_BOARD_HIT_CHECK", 1);
+
+		// 목록
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("start", page*10-9); 	//시작위치
+		map.put("end", page*10);		//종료위치	
+		map.put("text", text);			//검색어
+		List<BoardVO> list = bDAO.selectBoard(map);
+		model.addAttribute("list", list);
+	
+		
+		// 게시물 개수
+		int cnt = bDAO.countBoard(text); //검색어를 넘겨줌.
+		//System.out.println( (int) Math.ceil(n/10.0) );
+		model.addAttribute("cnt", (cnt-1)/10+1);
+		return request.getContextPath()+"/board/list";
+	}
+	
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
+	public String update(HttpServletRequest request,
+			Model model,
+			@RequestParam(value="no", defaultValue = "0") int no) {
+		BoardVO vo = bDAO.selectBoardOne(no);
+		model.addAttribute("vo", vo);
+		return request.getContextPath()+"/board/update";
+	}
+
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	public String update(HttpServletRequest request,
+			@ModelAttribute BoardVO obj,
+			@RequestParam MultipartFile[] img) throws IOException {  //변수명 == <input name="img"
+		//이미지는 수동으로 obj에 추가함.
+		if(img != null) {
+			for ( MultipartFile one : img   ) {
+				if(one.getSize() > 0) { //첨부한 파일의 용량이 있느냐?
+				//if(!one.getOriginalFilename().equals("")) { //파일명이 비어 있지 않다면
+					obj.setBrdimg( one.getBytes() );
+				}
+			}
+		}
+		
+		bDAO.updateBoard(obj);
+		return "redirect:" + request.getContextPath() + "/board/content?no="+obj.getBrdno();
+	}
+	
+
+	@RequestMapping(value="/getimg")
+	public ResponseEntity<byte[]> getimg(
+			@RequestParam("no") int no,
+			HttpServletRequest request){
+		BoardVO obj = bDAO.selectBoardImg(no);
+		try {
+			if (obj.getBrdimg().length > 0) { //이미지가 있음
+				HttpHeaders header = new HttpHeaders();
+				header.setContentType(MediaType.IMAGE_JPEG);
+				ResponseEntity<byte[]> ret = new ResponseEntity<byte[]>(
+							obj.getBrdimg(), header, HttpStatus.OK);
+				return ret;
+			}
+			return null;
+		}
+		catch(Exception e) {
+			try {
+				//request.getServletContext().getResourceAsStream() ==	//src/main/webapp
+				InputStream in 
+					= request.getServletContext().getResourceAsStream("/resources/img/default.jpg");
+				HttpHeaders header = new HttpHeaders();
+				header.setContentType(MediaType.IMAGE_JPEG);
+				ResponseEntity<byte[]> ret = new ResponseEntity<byte[]>(
+							IOUtils.toByteArray(in), header, HttpStatus.OK);
+				return ret;
+			}
+			catch(Exception e1) {
+				return null;
+			}
+		}
+	}
+	
+	
 }
--- demo/src/main/java/com/soulmovie/dao/BoardDAO.java
+++ demo/src/main/java/com/soulmovie/dao/BoardDAO.java
@@ -1,5 +1,9 @@
 package com.soulmovie.dao;
 
+import java.util.HashMap;
+
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +16,42 @@ import com.soulmovie.vo.BoardVO;
 public class BoardDAO {
 
 	@Autowired 
-	private SqlSessionFactory sqlFatory = null;
+	private SqlSessionFactory sqlFactory = null;
 	
 	public int insertBoard(BoardVO obj) {
-		return sqlFatory.openSession().insert("Board.insertBoard", obj);
+		return sqlFactory.openSession().insert("Board.insertBoard", obj);
+	}
+
+	public int updateHit(int no) {
+		return sqlFactory.openSession().update("Board.updateHit", no);
+	}
+
+	public BoardVO selectBoardOne(int no) {
+		return sqlFactory.openSession().selectOne("Board.selectBoardOne", no);
+	}
+	
+	public int selectBoardPrev(int no) {
+		return sqlFactory.openSession().selectOne("Board.selectBoardPrev", no);
+	}
+	
+	public int selectBoardNext(int no) {
+		return sqlFactory.openSession().selectOne("Board.selectBoardNext", no);
+	}
+	
+	public List<BoardVO> selectBoard(HashMap<String, Object> map) {
+		return sqlFactory.openSession().selectList("Board.selectList", map);
+	}
+	
+	public int countBoard(String text) {
+		return sqlFactory.openSession().selectOne("Board.count", text);
+	}
+	
+	public int updateBoard(BoardVO obj) {
+		return sqlFactory.openSession().update("Board.updateBoardOne", obj);
+	}
+
+	public BoardVO selectBoardImg(int no) {
+		return sqlFactory.openSession().selectOne("Board.selectBoardImg", no);
 	}
 }
 
